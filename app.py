dcf4cf8250f05d3e22e41f3c5648c4840bd4014b
punithkumar686-sketch <punithkumar686@gmail.com>
2026-04-26 16:13:29 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
