279e4fc79084ed7de3df0f390e2d30198e27a2d7
punithkumar686-sketch <punithkumar686@gmail.com>
2026-04-26 16:05:35 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
@@ -7,4 +8,5 @@ def home():
     return "🔥 Working!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=True)
+    port = 8000
+    app.run(host="0.0.0.0", port=port, debug=True)
